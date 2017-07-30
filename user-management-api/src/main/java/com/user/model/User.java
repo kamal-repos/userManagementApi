4f8bd9799a760a30785507686d9b732ed9fc9d23
Kamal <bounapallikamalakar@gmail.com>
2017-07-30 16:59:31 -0500

    Adding user model

---
+++ user-management-api/src/main/java/com/user/model/User.java
@@ -0,0 +1,60 @@
+package com.user.model;
+
+public class User {
+
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private String emailId;
+	private Long phoneNumber;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public Long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(Long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
